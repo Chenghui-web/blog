c4027ec515276a81bb0880d601257ba4c837a869
chengxiaoxuan <462560628@qq.com>
2018-05-23 17:33:53 +0800

    introduce

--- src/components/index/HelloWorld.vue
+++ src/components/index/HelloWorld.vue
@@ -2,11 +2,51 @@
   <div class="contain">
     <div class="top-contain">
       <div class="top-banner">
-      <!-- <img src="http://www.gravatar.com/avatar/bc4a0fc2887a8c692c77e42011b7c4a6" /> -->
+        <div class="title">
+          程晓煊の空间
+        </div>
+        <div class="module-contain">
+          <div class="moduler">
+            <div class="module-common">
+              笔记
+            </div>
+            <div class="module-common">
+              简介
+            </div>
+            <div class="module-common">
+              角落
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="welcome">
+        <div class="border-before"></div>
+        <span class='span-title'>Welcome to my home    ↓</span>
+        <div class="border-after"></div>
+      </div>
+    </div>
+    <div class="main-contain">
+      <div class="main-left-contain">
+        <div class="introduce">
+          <div class="introduce-title">
+            <div class="in-title">个人简介</div>
+            <img src="https://www.gravatar.com/avatar/bc4a0fc2887a8c692c77e42011b7c4a6" alt="" class='header-img'>
+            <div class="introduce-name">
+              程晓煊
+            </div>
+            <div class="say">Good morning</div>
+          </div>
+          <div class="introduct-main">
+            
+          </div>
+        </div>
+      </div>
+      <div class="main-right-contain">
+
       </div>
     </div>
 
-    
   </div>
 </template>
 
