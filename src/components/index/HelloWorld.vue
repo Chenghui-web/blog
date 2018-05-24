6c5a152dc2e75577e4217f27b80b4d2eb5acd304
chengxiaoxuan <462560628@qq.com>
2018-05-24 18:20:31 +0800

    main

--- src/components/index/HelloWorld.vue
+++ src/components/index/HelloWorld.vue
@@ -38,12 +38,47 @@
             <div class="say">Good morning</div>
           </div>
           <div class="introduct-main">
-            
+            <div class="contact">
+              <div class="contact-list">
+                 <span class='contact-title'>职业：</span><span class='descript'>前端工程师</span>
+              </div>
+              <div class="contact-list">
+                <span class='contact-title'>职龄：</span><span class='descript'>2年</span>
+              </div>
+              <div class="contact-list">
+                 <span class='contact-title'>微信：</span><span class='descript'>ch6080E</span>
+              </div>
+              <div class="contact-list">
+                 <span class='contact-title'>邮箱：</span><span class='descript'>462560628@qq.com</span>
+              </div>
+            </div>
+
+          </div>
+          <div class="bei">
+              <a href="http://www.miitbeian.gov.cn">苏ICP备18004998号</a>
+          </div>
+        </div>
+      </div>
+      <div class="main-center-contain">
+        <div class="center-main-title">
+          最近笔记
+        </div>
+        <div class="center-main">
+          <div class="mark-list">
+            <div class="mark-img"><img src="https://www.gravatar.com/avatar/bc4a0fc2887a8c692c77e42011b7c4a6" alt=""></div>
+            <div class="mark-main">
+              <div class="title">标题1</div>
+              <div class="mark-describe">这里是内容</div>
+              <div class="mark-msg">2018-5-15</div>
+            </div>
           </div>
         </div>
       </div>
       <div class="main-right-contain">
 
+        <div class="right-main">
+          管理后台   
+        </div>
       </div>
     </div>
 
@@ -52,5 +87,6 @@
 
 <style lang="less" scoped src="./hello.less"></style>
 <script src='./hello.js'></script>
+<script src='./can_co.js'></script>
 
 
