320f3b275f3871fcd39b2bba9069b38e08175c81
chengxiaoxuan <462560628@qq.com>
2018-05-30 18:20:12 +0800

    route

--- src/components/manage/manage.js
+++ src/components/manage/manage.js
@@ -1,18 +1,15 @@
 // import vue from "vue";
-import {mavonEditor} from "mavon-editor";
-import "mavon-editor/dist/css/index.css";
-import './can_co.js'
 export default {
-  name: "HelloWorld",
+  name: "manage",
 
   data() {
     return {
-      msg: "Welcome to Your Vue.js App123",
+      msg: "Welcome",
       value: ""
     };
   },
   components: {
-    mavonEditor
+    // mavonEditor
   },
   mounted(){
     // console.log(mavonEditor);
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/index/HelloWorld'
-
+import HelloWorld from '@/components/index/HelloWorld.vue'
+import manage from '@/components/manage/manage.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/manage',
+      name: 'manage',
+      component: manage
+    },
   ]
 })
